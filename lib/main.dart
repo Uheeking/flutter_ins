9e83ecb5b7f0796f65927897e304fff42266a4b8
Uheeking <wert1847@naver.com>
2023-08-25 17:53:51 +0900

    ADD : url에서 정보 가져오기

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,112 @@
 import 'package:flutter/material.dart';
+import 'style.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(theme: theme, home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class ActionsIconTheme {}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0;
+  var data = [];
+
+  getData() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    setState(() {
+      data = result2;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+        ),
+        actions: const [Icon(Icons.add_box_outlined)],
+      ),
+      body: tab == 0 ? Home(data: data) : Text('샵페이지'),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        onTap: (i) {
+          setState(() {
+            tab = i;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              label: '홈',
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: '샵',
+              icon: Icon(Icons.shopping_bag_outlined),
+              activeIcon: Icon(Icons.shopping_bag))
+        ],
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key, this.data});
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    if (data.isNotEmpty) {
+      return ListView.builder(
+          itemCount: 3,
+          itemBuilder: (c, i) {
+            return Column(
+              children: [
+                Container(
+                  constraints: BoxConstraints(maxWidth: 600),
+                  padding: EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(data[i]['image']),
+                      Text('좋아요 ${data[i]['likes']}'),
+                      Text('글쓴이 ${data[i]['user']}'),
+                      Text('글내용 ${data[i]['content']}'),
+                    ],
+                  ),
+                )
+              ],
+            );
+          });
+    } else {
+      return Center(
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: Column(
+            children: const [CircularProgressIndicator(), Text('로딩중입니다. ')],
+          ),
+        ),
+      );
+    }
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var theme = ThemeData(
+    bottomNavigationBarTheme:
+        BottomNavigationBarThemeData(selectedItemColor: Colors.black),
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      titleTextStyle: TextStyle(color: Colors.black),
+      actionsIconTheme: IconThemeData(color: Colors.black),
+    ));
